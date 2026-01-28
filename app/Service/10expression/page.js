42c8756b3c456d6649df7ad7c465760c3b78a5ce
parmarshubham305 <parmarshubham305@gmail.com>
2026-01-28 20:04:20 +0530

    design added

--- app/Service/10expression/page.js
+++ app/Service/10expression/page.js
@@ -226,7 +226,7 @@ export default function DashboardPage() {
               {result.output?.images?.[0]?.url && (
                 <div className="media-item">
                   <h4 className="media-heading">Generated Images</h4>
-                  <div className="flex flex-wrap gap-4">
+                  <div className="flex flex-wrap gap-4 expression-group">
                      <div className="relative group">
                        <Image src={result.output.images[0].url} alt="Generated Image" className="expression-image" width={200} height={200} unoptimized={true}
                             priority={true}
@@ -300,7 +300,9 @@ export default function DashboardPage() {
                        </div>
                      </div>
                      <div className="relative group">
-                       <Image src={result.output.images_7[0].url} alt="Generated Image" className="expression-image" width={200} height={200}/>
+                       <Image src={result.output.images_7[0].url} alt="Generated Image" className="expression-image" width={200} height={200} unoptimized={true}
+                            priority={true}
+                            onError={() => console.error("Failed to load generated image")}/>
                        <div className="absolute top-2 right-2 cursor-pointer bg-black p-3 rounded mr-1 download-btn" onClick={() => handleDownload(result.output.images_7[0].url, 'expression-7.jpg')}>
                          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                            <path d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
